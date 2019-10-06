83fde3ced8f06ad01da2f55056448fa0c2ba114c
Potato <42198534+Infinitestudy@users.noreply.github.com>
2019-10-06 22:35:18 +0800

    Update TimeoutInterval.py

--- code/TimeoutInterval.py
+++ code/TimeoutInterval.py
@@ -1,8 +1,11 @@
 # Computer Networking Problem P31
 
-#  Suppose that the five measured SampleRTT values (see Section 3.5.3 ) are 106 ms, 120
-# out
-# ms, 140 ms, 90 ms, and 115 ms. Compute the EstimatedRTT after each of these SampleRTT values is obtained, using a value of  and assuming that the value of EstimatedRTT was 100 ms just before the first of these five samples were obtained. Compute also the DevRTT after each sample is obtained, assuming a value of  and assuming the value of DevRTT was 5 ms just before the first of these five samples was obtained. Last, compute the TCP TimeoutInterval after each of these samples is obtained.
+# Suppose that the five measured SampleRTT values (see Section 3.5.3 ) are 106 ms, 120 ms, 140 ms, 90 ms, and 115 ms. 
+# Compute the EstimatedRTT after each of these SampleRTT values is obtained, using a value of alpha=0.125
+# and assuming that the value of EstimatedRTT was 100 ms just before the first of these five samples were obtained. 
+# Compute also the DevRTT after each sample is obtained, assuming a value of beta=0.25 
+# and assuming the value of DevRTT was 5 ms just before the first of these five samples was obtained. 
+# Last, compute the TCP TimeoutInterval after each of these samples is obtained.
 
 
 alpha = 0.125
